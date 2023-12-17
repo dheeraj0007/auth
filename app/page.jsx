dd0b29e23bc75fe2f53c1d7738ee6a76be396ee6
dheeraj0007 <dheeraj1863.be20@chitkara.edu.in>
2023-12-17 14:22:03 +0530

    Final Auth

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
 import LoginForm from "../components/LoginForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/dashboard");
   return (
     <div>
       <LoginForm />
     </div>
   );
-}
+};
+
+export default Home;
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,5 +1,11 @@
 import RegisterForm from "../../components/RegisterForm";
-const Register = () => {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/api/auth/[...nextauth]/route";
+
+const Register = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/dashboard");
   return <RegisterForm />;
 };
 
