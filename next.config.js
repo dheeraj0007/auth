fff99eba1ccb68c0ad2d3faf966a26cf06d74819
dheeraj0007 <dheeraj1863.be20@chitkara.edu.in>
2023-12-17 17:51:30 +0530

    Checking

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+    },
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
